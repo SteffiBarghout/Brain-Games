9a1fe6599d6d66c450d1352028a02d07e9b1dca1
Steffi Wellington <steffisbootcampdrive@gmail.com>
2020-05-09 23:33:25 -0700

    orm.js ? insertion function

--- config/orm.js
+++ config/orm.js
@@ -1,7 +1,15 @@
 // Import MySQL connection.
 var connection = require("../config/connection.js");
 
+//SQL syntax function to put in question marks for safety to fend off insertion malware ex: ; Drop table;
+function printQuestionMarks(num) {
+    var arr = [];
 
+    for (let i = 0; i < num.length; i++) {
+        arr.push("?");
+    }
+    return arr.toString();
+}
 
 //Export the orm object 
 module.exports = orm;

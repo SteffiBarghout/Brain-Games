71a700e38eb6393836874e2f93a4279dd6adf3a1
Steffi Wellington <steffisbootcampdrive@gmail.com>
2020-05-10 00:03:23 -0700

    models braingames.js setup

--- models/braingames.js
+++ models/braingames.js
@@ -1,1 +1,23 @@
 var orm = require("../config/orm.js");
+
+var puzzles = {
+    insertAll: function(cb) {
+        orm.insertAll("puzzles", function(res) {
+            cb(res);
+        });
+    },
+    // The variables cols and vals are arrays.
+    insertOne: function(cols, vals, cb) {
+        orm.InsertOne("puzzles", cols, vals, function(res) {
+            cb(res);
+        });
+    },
+    updateOne: function(objColVals, condition, cb) {
+        orm.updateOne("puzzles", objColVals, condition, function(res) {
+            cb(res);
+        });
+    }
+};
+
+// Export the database functions for the controller.
+module.exports = puzzles;
